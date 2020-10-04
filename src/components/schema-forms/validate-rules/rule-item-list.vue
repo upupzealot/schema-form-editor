7af2689ef4c2c1300c76436dc3ab6d366e590e83
upupzealot <hzxjhspxn@163.com>
2020-10-05 02:53:14 +0800

    update: 抽象 rule-item-list

---
+++ src/components/schema-forms/validate-rules/rule-item-list.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <el-form-item :label="label">
+      <el-button @click="createItem">
+        新增{{label}}校验
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <DraggableList
+        style="margin-left: -40px;"
+        :list="rules"
+      >
+        <DraggableListItem
+          v-for="rule in ruleItems"
+          :key="rule.id"
+          :item="rule"
+          @delete="deleteItem"
+        >
+          <slot
+            :field="field"
+            :rule="rule"
+          />
+        </DraggableListItem>
+      </DraggableList>
+    </el-form-item>
+  </div>
+</template>
+
+<script>
+import DraggableList  from '@/components/common/draggable-list'
+import DraggableListItem  from '@/components/common/draggable-list-item'
+
+import ruleMinxin from './rule-mixin'
+
+export default {
+  components: {
+    DraggableList,
+    DraggableListItem,
+  },
+  mixins: [ruleMinxin],
+  props: {
+    type: {
+      type: String,
+      default() {
+        return '';
+      }
+    },
+    label: {
+      type: String,
+      default() {
+        return '';
+      }
+    }
+  },
+  computed: {
+    ruleItems: {
+      get() {
+        return this.rules.filter(r => r.type === this.type)
+      },
+      set(rules) {
+        this.rules = ruleItems.filter(ri => ri.type !== this.type).concat(rules);
+      }
+    }
+  },
+  methods: {
+    createItem() {
+      const id = this.$id();
+      this.rules = [
+        ...this.rules, {
+        id,
+        type: this.type,
+      }];
+    },
+    deleteItem(rule) {
+      this.$confirm('确认删除?', null, {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'danger',
+      }).then(() => {
+        this.rules = this.rules.filter(r => r.id !== rule.id);
+
+        this.$message({
+          type: 'success',
+          message: '已删除',
+        });
+      }).catch(() => {});
+    },
+  }
+}
+</script>
