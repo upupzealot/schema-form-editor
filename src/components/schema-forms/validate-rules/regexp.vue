c266b990ae6bd7c784bda75e40afc86265463ae9
eric <hzxjhspxn@163.com>
2020-10-05 02:53:14 +0800

    update: 抽象 rule-item-list

--- src/components/schema-forms/validate-rules/regexp.vue
+++ src/components/schema-forms/validate-rules/regexp.vue
@@ -1,78 +1,19 @@
 <template>
-  <div>
-    <el-form-item label="正则">
-      <el-button @click="createRule">
-        新增校验规则
-      </el-button>
-    </el-form-item>
-    <el-form-item>
-      <DraggableList
-        style="margin-left: -40px;"
-        :list="rules"
-      >
-        <DraggableListItem
-          v-for="rule in regexpRules"
-          :key="rule.id"
-          :item="rule"
-          @delete="deleteRule"
-        >
-          <RegexpRule
-            :field="field"
-            :rule="rule"
-          />
-        </DraggableListItem>
-      </DraggableList>
-    </el-form-item>
-  </div>
+  <RuleItemList label="正则" type="regexp">
+    <template v-slot:default="{ field, rule }">
+      <RegexpItem :field="field" :rule="rule" />
+    </template>
+  </RuleItemList>
 </template>
 
 <script>
-import DraggableList  from '@/components/common/draggable-list'
-import DraggableListItem  from '@/components/common/draggable-list-item'
-
-import ruleMinxin from './rule-mixin'
-import RegexpRule from './regexp-rule'
+import RuleItemList from './rule-item-list'
+import RegexpItem from './regexp-item'
 
 export default {
   components: {
-    DraggableList,
-    DraggableListItem,
-    RegexpRule
+    RuleItemList,
+    RegexpItem,
   },
-  mixins: [ruleMinxin],
-  computed: {
-    regexpRules: {
-      get() {
-        return this.rules.filter(r => r.type === 'regexp')
-      },
-      set(rules) {
-        this.rules = regexpRules.filter(r => r.type !== 'regexp').concat(rules);
-      }
-    }
-  },
-  methods: {
-    createRule() {
-      const id = this.$id();
-      this.rules = [
-        ...this.rules, {
-        id,
-        type: 'regexp',
-      }];
-    },
-    deleteRule(rule) {
-      this.$confirm('确认删除?', null, {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'danger',
-      }).then(() => {
-        this.rules = this.rules.filter(r => r.id !== rule.id);
-
-        this.$message({
-          type: 'success',
-          message: '已删除',
-        });
-      }).catch(() => {});
-    },
-  }
 }
 </script>
