d1a707396215472ef1ce57327ffd8eb45cef0dbb
upupzealot <hzxjhspxn@163.com>
2020-10-26 15:53:49 +0800

    update: 多项目保存

--- src/views/portal.vue
+++ src/views/portal.vue
@@ -1,5 +1,8 @@
 <template>
   <el-container>
+    <el-header>
+      <ProjectSelect />
+    </el-header>
     <el-main>
       <el-row :gutter="20">
         <el-col :span="6">
@@ -33,6 +36,8 @@
 <script>
 import md5 from 'md5';
 
+import ProjectSelect from '@/components/project-select'
+
 import ItemBar from '@/components/item-bar';
 // 全局引入，避免循环引用
 // import FormEditor from '@/components/form-editor';
@@ -40,6 +45,7 @@ import ItemEditor from '@/components/item-editor';
 
 export default {
   components: {
+    ProjectSelect,
     ItemBar,
     // FormEditor,
     ItemEditor,
@@ -58,40 +64,21 @@ export default {
   mounted() {
     const self = this;
 
+    // 绑定“保存”键盘事件
     document.onkeydown = function(e) {
       if(e.keyCode === 83 && (e.ctrlKey || e.metaKey)) {
         e.preventDefault();
         e.stopPropagation();
 
         const schema = self.$refs['rootEditor'].schema;
-        localStorage.setItem('schema', JSON.stringify(schema));
+        const projectId = localStorage.getItem('projectId') || 'default';
+        localStorage.setItem(projectId, JSON.stringify(schema));
         self.$message({
           message: '保存成功',
           type: 'success'
         });
       }
     };
-
-    const schemaStr = localStorage.getItem('schema');
-    if(schemaStr && schemaStr !== 'undefined') {
-      const schema = JSON.parse(schemaStr);
-
-      let count = 0;
-      function addId(form) {
-        if(form.fieldList) {
-          form.fieldList.forEach(field => {
-            field.id = md5(`${Date.now()}${count}`).substr(0, 7);
-            count++;
-            addId(field);
-          })
-        }
-      }
-      addId(schema);
-
-      this.$store.commit('$root/setFormConf', schema.formConf);
-      this.$store.commit('$root/setFieldList', schema.fieldList);
-      this.$store.commit('$root/setValidRules', schema.validRules);
-    }
-  }
+  },
 };
 </script>
