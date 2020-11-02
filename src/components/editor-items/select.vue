d8d05743d1c7ef02f0ec11274779c2714e8c1795
eric <hzxjhspxn@163.com>
2020-11-02 10:31:12 +0800

    fix

--- src/components/editor-items/select.vue
+++ src/components/editor-items/select.vue
@@ -5,17 +5,21 @@
     :v-status="vStatus"
     :sup-nodes="supNodes"
     :class="statusClasses"
-    class="select-input-item"
+    class="editor-select-item"
   />
 </template>
 
 <style>
-.select-input-item.v-readonly .el-form-item__content {
+.editor-select-item.v-readonly .el-form-item__content {
   background: linear-gradient(45deg, #efefef 25%, transparent 0, transparent 50%, #efefef 0, #efefef 75%, transparent 0);
   background-size:20px 20px;
   
 }
-.select-input-item.v-readonly .el-form-item__content .el-select {
+.editor-select-item.v-disabled .el-form-item__content {
+  background-color: #efefef;
+}
+.editor-select-item.v-disabled .el-form-item__content .el-select,
+.editor-select-item.v-readonly .el-form-item__content .el-select {
   opacity: .6;
 }
 </style>
