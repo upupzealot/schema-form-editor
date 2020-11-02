296d7afff2b494be88232ac346986d705199dee1
eric <hzxjhspxn@163.com>
2020-11-02 16:01:56 +0800

    update: checkbox 组件四状态

---
+++ src/components/editor-items/checkbox.vue
@@ -0,0 +1,22 @@
+<template>
+  <Checkbox
+    :field="field"
+    :data="data"
+    :v-status="vStatus"
+    :sup-nodes="supNodes"
+    :class="statusClasses"
+  />
+</template>
+
+<script>
+import standardMixin from './standard-mixin'
+
+import Checkbox from '../form-items/checkbox'
+
+export default {
+  components: {
+    Checkbox,
+  },
+  mixins: [standardMixin],
+}
+</script>
