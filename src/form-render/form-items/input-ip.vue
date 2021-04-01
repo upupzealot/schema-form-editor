cb2503e525f1b773aab0ca9ba311752dc6d78c2b
eric <hzxjhspxn@163.com>
2021-04-01 17:07:24 +0800

    update: 支持组件预设校验器

--- src/form-render/form-items/input-ip.vue
+++ src/form-render/form-items/input-ip.vue
@@ -59,41 +59,36 @@ import standardMixin from './standard-mixin'
 
 export default {
   mixins: [standardMixin],
-  data() {
-    const rules = [{
-      type: 'string',
-      validator: (r, value, callback) => {
-        const errMsg = '请输入正确的 IP 地址，每位 0~255 之间的整数';
-        const snippets = value && value.split('.');
-        if(snippets && snippets.length === 4) {
-          for(let i = 0; i < 4; i++) {
-            const snippet = snippets[i];
-            let pass = /[\d]+/.test(snippet);
-            if(!pass) {
-              callback(new Error(errMsg));
-              break;
-            }
-            const num = Number(snippet);
-            pass = num >= 0 && num <= 255;
-            if(!pass) {
-              callback(new Error(errMsg));
-              break;
-            }
-            if(!i && num === 0) {
-              callback(new Error('首位不能为 0'));
-            }
+  validators: [{
+    name: 'IP',
+    key: 'ip',
+    func: (r, value, callback) => {
+      const errMsg = '请输入正确的 IP 地址，每位 0~255 之间的整数';
+      const snippets = value && value.split('.');
+      if(snippets && snippets.length === 4) {
+        for(let i = 0; i < 4; i++) {
+          const snippet = snippets[i];
+          let pass = /[\d]+/.test(snippet);
+          if(!pass) {
+            callback(new Error(errMsg));
+            break;
           }
-        } else {
-          callback(new Error(errMsg));
+          const num = Number(snippet);
+          pass = num >= 0 && num <= 255;
+          if(!pass) {
+            callback(new Error(errMsg));
+            break;
+          }
+          if(!i && num === 0) {
+            callback(new Error('首位不能为 0'));
+          }
         }
-        callback();
+      } else {
+        callback(new Error(errMsg));
       }
-    }]
-
-    return {
-      rules,
+      callback();
     }
-  },
+  }],
   computed: {
     ip0: {
       get() { return this.getSnippet(0) },
