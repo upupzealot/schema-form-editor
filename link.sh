e58ac0ee0b724675cccee1318267eefc4fe51964
eric <hzxjhspxn@163.com>
2021-04-02 16:24:19 +0800

    update: switch of vue/vuex/elementui

---
+++ link.sh
@@ -0,0 +1,11 @@
+# link vue
+cd ./node_modules/vue2
+npm link --production
+cd ../../
+npm link vue
+
+# link vuex
+cd ./node_modules/vuex3
+npm link --production
+cd ../../
+npm link vuex
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const processArgv = process.argv;
+const isVue3 = processArgv.includes('--vue3');
+process.env.IS_VUE3 = isVue3;
+
 module.exports = {
+  pages: {
+    app: {
+      entry: isVue3 ? 'src/main3.js' : 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    },
+  },
   lintOnSave: false,
 };
