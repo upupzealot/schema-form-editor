3c07772a9441b936b20f6e855571c630320f9ac9
upupzealot <hzxjhspxn@163.com>
2021-04-06 10:37:18 +0800

    update: 更新 link 脚本，避免重复切换

--- link.sh
+++ link.sh
@@ -1,17 +1,21 @@
-# link vue
-cd ./node_modules/vue2
-npm link --production
-cd ../../
-npm link vue
+isVue2=`cat ./node_modules/vue/package.json | grep "\"version\": \"2"`
 
-# link vuex
-cd ./node_modules/vuex3
-npm link --production
-cd ../../
-npm link vuex
+if [ -z "$isVue2" ]; then
+  # link vue
+  cd ./node_modules/vue2
+  npm link --production
+  cd ../../
+  npm link vue
 
-# link vuedraggable
-cd ./node_modules/vuedraggable2
-npm link --production
-cd ../../
-npm link vuedraggable
+  # link vuex
+  cd ./node_modules/vuex3
+  npm link --production
+  cd ../../
+  npm link vuex
+
+  # link vuedraggable
+  cd ./node_modules/vuedraggable2
+  npm link --production
+  cd ../../
+  npm link vuedraggable
+fi
