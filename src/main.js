a04b14cf877e75b715d2199173f725a858914e0c
upupzealot <hzxjhspxn@163.com>
2021-04-14 16:24:56 +0800

    update: form-render 遵循 uikit 切换

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ const clipboardy = require('clipboardy');
 
 import App from './portal.vue';
 import FormEditor from '@/framework/form-editor.vue'
-import FormRender from '@/ui-kit/element-ui/form-render.vue'
+import ElementFormRender from '@/ui-kit/element-ui/form-render.vue'
+import AntDesignFormRender from '@/ui-kit/ant-design/form-render.vue'
 import CodeEditor from '@/framework/common/code-editor.vue'
 import DraggableList from '@/ui-kit/common/draggable-list';
 import DraggableListItem from '@/ui-kit/common/draggable-list-item';
@@ -21,13 +22,20 @@ Vue.config.productionTip = false;
 Vue.prototype.$id = $id;
 Vue.prototype.$clipboard = clipboardy;
 
-Vue.use(ElementUI);
 Vue.use(AntDesignVue);
+Vue.use(ElementUI); // ElementUI 放在最后引入，保证类似 $message 的方法最终可用
 
 Vue.component('CodeEditor', CodeEditor);
 Vue.component('DraggableList', DraggableList);
 Vue.component('DraggableListItem', DraggableListItem);
-Vue.component('FormRender', FormRender);
+
+const uiKit = localStorage.getItem('ui-kit');
+if(uiKit === 'element-ui') {
+  Vue.component('FormRender', ElementFormRender);
+}
+if(uiKit === 'ant-design') {
+  Vue.component('FormRender', AntDesignFormRender);
+}
 Vue.component('FormEditor', FormEditor);
 
 new Vue({
