7c3b4bc6e0c2796f5b81a0f12f6b658c2514e51f
upupzealot <hzxjhspxn@163.com>
2021-06-29 10:08:09 +0800

    fix: link 脚本 stdio

--- link.js
+++ link.js
@@ -7,25 +7,29 @@ let isVue2 = version.startsWith('2.');
 if(!isVue2) {
   console.log('linking: vue@2')
   execSync('npm link --production', {
-    cwd: './node_modules/vue2'
+    cwd: './node_modules/vue2',
+    stdio: 'inherit',
   });
   execSync('npm link vue');
 
   console.log('linking: vuex@3')
   execSync('npm link --production', {
-    cwd: './node_modules/vuex3'
-  })
+    cwd: './node_modules/vuex3',
+    stdio: 'inherit',
+  });
   execSync('npm link vuex');
 
   console.log('linking: vuedraggable2')
   execSync('npm link --production', {
-    cwd: './node_modules/vuedraggable2'
-  })
+    cwd: './node_modules/vuedraggable2',
+    stdio: 'inherit',
+  });
   execSync('npm link vuedraggable');
 
   console.log('linking: ant-design-vue1')
   execSync('npm link --production', {
-    cwd: './node_modules/ant-design-vue1'
-  })
+    cwd: './node_modules/ant-design-vue1',
+    stdio: 'inherit',
+  });
   execSync('npm link ant-design-vue');
 }
--- link3.js
+++ link3.js
@@ -7,25 +7,29 @@ let isVue3 = version.startsWith('3.');
 if(!isVue3) {
   console.log('linking: vue@3')
   execSync('npm link --production', {
-    cwd: './node_modules/vue3'
+    cwd: './node_modules/vue3',
+    stdio: 'inherit',
   });
   execSync('npm link vue');
 
   console.log('linking: vuex@4')
   execSync('npm link --production', {
-    cwd: './node_modules/vuex4'
-  })
+    cwd: './node_modules/vuex4',
+    stdio: 'inherit',
+  });
   execSync('npm link vuex');
 
   console.log('linking: vuedraggable4')
   execSync('npm link --production', {
-    cwd: './node_modules/vuedraggable4'
-  })
+    cwd: './node_modules/vuedraggable4',
+    stdio: 'inherit',
+  });
   execSync('npm link vuedraggable');
 
   console.log('linking: ant-design-vue2')
   execSync('npm link --production', {
-    cwd: './node_modules/ant-design-vue2'
-  })
+    cwd: './node_modules/ant-design-vue2',
+    stdio: 'inherit',
+  });
   execSync('npm link ant-design-vue');
 }
