9a06cbd9bec2b8a8d4e20aeec6dac0b49d8146c2
upupzealot <hzxjhspxn@163.com>
2021-07-23 11:24:23 +0800

    update: 更新包装工具函数 & full-demo -> basic

--- test/unit/_setup_/setup.common.js
+++ test/unit/_setup_/setup.common.js
@@ -1,7 +1,13 @@
-import { mount } from '@vue/test-utils'
 import _ from 'lodash';
+import { mount, Wrapper } from '@vue/test-utils'
 
 global._ = _;
 global.wrap = (option) => {
-  return mount(SchemaFormRender, option);
+  let opt = option;
+  if(!option.propsData) {
+    opt = {
+      propsData: option,
+    };
+  }
+  return mount(SchemaFormRender, opt);
 }
---
+++ test/unit/basic.spec.js
@@ -0,0 +1,11 @@
+import TestSchema from '../test.schema.json'
+
+describe('基本渲染', () => {
+  const wrapper = wrap({
+    schema: TestSchema,
+  });
+
+  test('全家桶渲染', () => {
+    expect(wrapper.vm).toBeTruthy();
+  });
+})
