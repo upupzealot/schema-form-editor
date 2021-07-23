549535f0d0c93d123acf6679c7b8daeac83bbacf
upupzealot <hzxjhspxn@163.com>
2021-07-23 14:46:15 +0800

    update: change cmd

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,7 +1,8 @@
-const { execSync } = require('child_process')
+const { execSync } = require('child_process');
 
-const isElementUi = process.argv.length === 2 || process.argv.includes('ele');
-const isAntDesign = process.argv.length === 2 || process.argv.includes('ant');
+const isElementUi = process.argv.length === 2 || process.argv.includes('--ele');
+const isAntDesign = process.argv.length === 2 || process.argv.includes('--ant');
+
 if(isElementUi) {
   console.log();
   execSync('jest --verbose --coverage=false --config=jest-element-ui.config.js', {
