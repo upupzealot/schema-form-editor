c74d10d388d1e968f6935f6ca8f726ce0a1d59f9
upupzealot <hzxjhspxn@163.com>
2021-07-26 18:44:31 +0800

    update: 标签宽度 UI 测试

---
+++ test/unit/form-config/label-width.spec.js
@@ -0,0 +1,65 @@
+import { boundOf } from '../util'
+import LabelWidthSchema from '../../test.schema.json'
+
+describe('标签宽度', () => {
+  const wrapper = wrap({
+    schema: LabelWidthSchema,
+  });
+
+  let page = null;
+  beforeAll(async () => {
+    page = await browser.newPage();
+    await page.goto(`http://localhost:${pagePort}/?schema=form-config/label-width`, {
+      waitUntil: 'networkidle0',
+    });
+    await delay(1000);
+  });
+
+  test('默认宽度 80px', async () => {
+    let label1 = null;
+    if(uikit === 'element-ui') {
+      label1 = await boundOf(page, '#app [sfr-f="input-1"] .el-form-item__label');
+    }
+    if(uikit === 'ant-design') {
+      label1 = await boundOf(page, '#app [sfr-f="input-1"] .ant-form-item-label');
+    }
+    expect(label1.width).toEqual(80);
+  });
+
+  test('设置为 100px', async () => {
+    await page.evaluate(()=>{
+      window.$form.schema.formConf = {
+        labelWidth: '100px'
+      }
+    });
+    await delay(1000);
+
+    let label1 = null;
+    if(uikit === 'element-ui') {
+      label1 = await boundOf(page, '#app [sfr-f="input-1"] .el-form-item__label');
+    }
+    if(uikit === 'ant-design') {
+      label1 = await boundOf(page, '#app [sfr-f="input-1"] .ant-form-item-label');
+    }
+    expect(label1.width).toEqual(100);
+  });
+
+  test('设置为 20%', async () => {
+    await page.evaluate(()=>{
+      window.$form.schema.formConf = {
+        labelWidth: '20%'
+      }
+    });
+    await delay(1000);
+
+    let input1 = await boundOf(page, '#app [sfr-f="input-1"]');
+    let label1 = null;
+    if(uikit === 'element-ui') {
+      label1 = await boundOf(page, '#app [sfr-f="input-1"] .el-form-item__label');
+    }
+    if(uikit === 'ant-design') {
+      label1 = await boundOf(page, '#app [sfr-f="input-1"] .ant-form-item-label');
+    }
+    expect(label1.width).toEqual(input1.width * 0.2);
+  });
+})
