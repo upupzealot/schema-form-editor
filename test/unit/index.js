98f723107c773c40939d835c762bdbdd8b1c3496
upupzealot <hzxjhspxn@163.com>
2021-07-28 15:24:03 +0800

    update: 脚本单独启动指定 test

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,17 +1,36 @@
 const { execSync } = require('child_process');
 
-const isElementUi = process.argv.length === 2 || process.argv.includes('--ele');
-const isAntDesign = process.argv.length === 2 || process.argv.includes('--ant');
+let args = process.argv.slice(2);
+
+const uikitFlags = ['--ele', '--ant'];
+let isUikitEmpty = true;
+uikitFlags.forEach(flag => {
+  isUikitEmpty = isUikitEmpty && !args.includes(flag);
+});
+
+const isElementUi = isUikitEmpty || process.argv.includes('--ele');
+const isAntDesign = isUikitEmpty || process.argv.includes('--ant');
+
+args = args.filter(arg => !uikitFlags.includes(arg));
+let pattern = '';
+if(args.length) {
+  pattern = args[0];
+  if(pattern.startsWith('/')) {
+    pattern = `--testPathPattern=${pattern}`
+  } else {
+    pattern = `--testNamePattern=${pattern}`
+  }
+}
 
 if(isElementUi) {
   console.log();
-  execSync('jest --verbose --coverage=false --config=jest-element-ui.config.js', {
+  execSync(`jest ${pattern} --verbose --coverage=false --config=jest-element-ui.config.js`, {
     stdio: 'inherit',
   });
 }
 if(isAntDesign) {
   console.log();
-  execSync('jest --verbose --coverage=false --config=jest-ant-design.config.js', {
+  execSync(`jest ${pattern} --verbose --coverage=false --config=jest-ant-design.config.js`, {
     stdio: 'inherit',
   });
 }
