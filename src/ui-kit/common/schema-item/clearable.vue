54dc6c1aea28919f5871d979bab839227cd3aa37
upupzealot <hzxjhspxn@163.com>
2021-07-29 09:39:43 +0800

    update: item-editor 通用项，clearable

---
+++ src/ui-kit/common/schema-item/clearable.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-form-item
+    label="可清空"
+    prop="clearable"
+    active-text="是"
+    inactive-text="否"
+  >
+    <el-switch
+      v-model="clearable"
+    />
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  props: {
+    field: {
+      type: Object,
+      default() {
+        return {};
+      }
+    },
+  },
+  computed: {
+    clearable: {
+      get() {
+        return !!this.field.clearable;
+      },
+      set(val) {
+        this.$set(this.field, 'clearable', val || undefined);
+      }
+    }
+  }
+}
+</script>
