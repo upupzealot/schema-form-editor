5ba4d2e58f75179614536a7926f9743d1697be2b
upupzealot <hzxjhspxn@163.com>
2021-08-04 20:04:41 +0800

    fix: 控制按钮测试 vue2 兼容

--- test/unit/components/input-number/control-buttons.spec.js
+++ test/unit/components/input-number/control-buttons.spec.js
@@ -41,17 +41,21 @@ describe('数字框：控制按钮', () => {
     })).rejects.toThrow(); //Error: failed to find element matching selector ...
   });
 
+  function Less1(num1, num2) {
+    expect(Math.abs(num2 - num1)).toBeLessThanOrEqual(1);
+  }
+
   antSkip('控制按钮在两侧', async () => {
     const input1 = await boundOf(page, '#app [sfr-f="input-number-1"] input');
     const incBtn = await boundOf(page, '#app [sfr-f="input-number-1"] .el-input-number__increase');
     const decBtn = await boundOf(page, '#app [sfr-f="input-number-1"] .el-input-number__decrease');
 
-    expect(decBtn.left).toEqual(input1.left);
-    expect(incBtn.right).toEqual(input1.right);
-    expect(decBtn.top).toEqual(input1.top);
-    expect(incBtn.top).toEqual(input1.top);
-    expect(decBtn.bottom).toEqual(input1.bottom);
-    expect(incBtn.bottom).toEqual(input1.bottom);
+    Less1(decBtn.left, input1.left);
+    Less1(incBtn.right, input1.right);
+    Less1(decBtn.top, input1.top);
+    Less1(incBtn.top, input1.top);
+    Less1(decBtn.bottom, input1.bottom);
+    Less1(incBtn.bottom, input1.bottom);
   });
 
   test('控制按钮在右侧', async () => {
@@ -63,10 +67,10 @@ describe('数字框：控制按钮', () => {
     #app [sfr-f="input-number-1"] .el-input-number__decrease,
     #app [sfr-f="input-number-1"] .ant-input-number-handler.ant-input-number-handler-down`);
 
-    expect(incBtn.top).toEqual(input1.top);
-    expect(decBtn.bottom).toEqual(input1.bottom);
-    expect(incBtn.right).toEqual(input1.right);
-    expect(decBtn.right).toEqual(input1.right);
-    expect(incBtn.left).toEqual(decBtn.left);
+    Less1(incBtn.top, input1.top);
+    Less1(decBtn.bottom, input1.bottom);
+    Less1(incBtn.right, input1.right);
+    Less1(decBtn.right, input1.right);
+    Less1(incBtn.left, decBtn.left);
   });
 })
