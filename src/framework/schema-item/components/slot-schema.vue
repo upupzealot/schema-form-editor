8bca357c020a734f935210cc7bb96979784c0078
upupzealot <hzxjhspxn@163.com>
2021-08-10 19:47:10 +0800

    update: 合并 slot-schema & 插槽组件增加校验

--- src/framework/schema-item/components/slot-schema.vue
+++ src/framework/schema-item/components/slot-schema.vue
@@ -1,5 +1,5 @@
 <template>
-  <StandardForm :config="{ hasValid: false }">
+  <StandardForm>
     <template v-slot:basic>
       <el-form-item
         label="插槽名称"
@@ -10,15 +10,27 @@
         />
       </el-form-item>
     </template>
+
+    <!-- 校验 -->
+    <template v-slot:valid>
+      <RegexpValidate />
+      <FuncValidate />
+    </template>
   </StandardForm>
 </template>
 
 <script>
 import schemaItemMixin from '@/framework/schema-item/mixin';
+import RegexpValidate from '@/framework/schema-item/common/validate-rules/regexp'
+import FuncValidate from '@/framework/schema-item/common/validate-rules/func'
 
 export default {
   type: 'slot',
   mixins: [schemaItemMixin],
+  components: {
+    RegexpValidate,
+    FuncValidate,
+  },
   computed: {
     slotName: {
       get() {
