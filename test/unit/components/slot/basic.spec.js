cb219aa2987df97096130c649c80cfb57824b257
upupzealot <hzxjhspxn@163.com>
2021-08-11 15:03:25 +0800

    fix

--- test/unit/components/slot/basic.spec.js
+++ test/unit/components/slot/basic.spec.js
@@ -2,7 +2,7 @@ describe('插槽', () => {
   let page = null;
   beforeAll(async () => {
     page = await browser.newPage();
-    await page.goto(`http://localhost:${pagePort}/?schema=components/slot/render&slotName=test&slotComponent=slot-test`, {
+    await page.goto(`http://localhost:${pagePort}/?schema=components/slot/basic&slotName=test&slotComponent=slot-test`, {
       waitUntil: 'networkidle0',
     });
     await delay(1000);
@@ -13,7 +13,7 @@ describe('插槽', () => {
       return !!$el;
     });
     expect(hasSlot).toBeTruthy();
-  })
+  });
 
   test('数据绑定', async () => {
     const innerText = await page.$eval(`#app [sfr-f="slot-1"] .el-form-item__content,
@@ -21,5 +21,5 @@ describe('插槽', () => {
       return $el.innerText;
     });
     expect(innerText).toEqual('Slot: slot-1');
-  })
+  });
 })
