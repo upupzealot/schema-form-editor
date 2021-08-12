3ce51698c11a277fa248c0b60ea83f7708f12a6a
upupzealot <hzxjhspxn@163.com>
2021-08-12 14:07:36 +0800

    test: slot 组件 UI 测试，更新回填测试用例

--- test/unit/components/slot/fillback.spec.js
+++ test/unit/components/slot/fillback.spec.js
@@ -14,4 +14,27 @@ describe('插槽：数据回填', () => {
     });
     expect(value).toEqual('aaa');
   });
+
+  test('容器插槽', async () => {
+    const value = await page.$eval('#app [sfr-f="wrapper-1"] [sfr-f="slot-2"] input', $el => {
+      return $el.value;
+    });
+    expect(value).toEqual('bbb');
+  });
+
+  test('子表单插槽', async () => {
+    const value = await page.$eval('#app [sfr-f="subform-1"] [sfr-f="slot-3"] input', $el => {
+      return $el.value;
+    });
+    expect(value).toEqual('ccc');
+  });
+
+  test('列表插槽', async () => {
+    for(let i = 0; i < 2; i++) {
+      const value = await page.$eval(`#app [sfr-f="item-list-1[${i}]"] [sfr-f="slot-4"] input`, $el => {
+        return $el.value;
+      });
+      expect(value).toEqual(`ddd-${i + 1}`);
+    }
+  });
 })
