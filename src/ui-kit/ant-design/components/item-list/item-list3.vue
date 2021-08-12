3400da88054252294ee90d4a5199db72827c52bc
upupzealot <hzxjhspxn@163.com>
2021-08-12 11:22:41 +0800

    update: vue3 下 item-list 组件适配

--- src/ui-kit/ant-design/components/item-list/item-list3.vue
+++ src/ui-kit/ant-design/components/item-list/item-list3.vue
@@ -32,13 +32,23 @@
         >
           <Subform
             ref="subformItem"
-            :field="field"
+            :field="subformField"
             :schema="subformSchema"
             :data="item"
             :sup-nodes="supNodes"
             :scenario="scenario"
             :style="{ width: '100%', marginBottom: `-${subformMarginY}px` }"
-          />
+          >
+            <template
+              v-for="slotName in slotNames"
+              v-slot:[slotName]="args"
+            >
+              <slot
+                :name="slotName"
+                v-bind="args"
+              />
+            </template>
+          </Subform>
         </DraggableListItem>
       </template>
     </DraggableList>
@@ -75,10 +85,11 @@
 </style>
 
 <script>
-import formItemMixin from '@/ui-kit/ant-design/common/form-item/mixin'
+import formItemMixin from '../../common/form-item/mixin'
+import itemListMixin from '../../../common/components/item-list-mixin'
 
-import DraggableList from '@/ui-kit/common/draggable-list3'
-import DraggableListItem from '@/ui-kit/common/draggable-list-item'
+import DraggableList from '../../../common/draggable-list3'
+import DraggableListItem from '../../../common/draggable-list-item'
 import Subform from '../subform/subform'
 
 export default {
@@ -88,77 +99,6 @@ export default {
     DraggableListItem,
     Subform
   },
-  mixins: [formItemMixin],
-  props: {
-    schema: {
-      type: Object,
-      default() {
-        return {};
-      }
-    }
-  },
-  computed: {
-    subformMarginY() {
-      return this.schema.formConf.marginY || 15;
-    },
-    editable() {
-      return !this.readonly && !this.disabled;
-    },
-    subformSchema() {
-      return {
-        ...this.field,
-        type: 'subform',
-      };
-    },
-    items: {
-      get() {
-        return this.data[this.field.name];
-      },
-      set(items) {
-        this.$set(this.data, this.field.name, items);
-      }
-    }
-  },
-  created() {
-    if(!this.items) {
-      this.items = [];
-    }
-  },
-  methods: {
-    addItem() {
-      this.items = [...this.items, {}];
-    },
-    deleteItem(item) {
-      this.$confirm('确认删除?', null, {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'danger',
-      }).then(() => {
-        this.items = this.items.filter(i => i !== item);
-
-        this.$message({
-          type: 'success',
-          message: '已删除',
-        });
-      }).catch(() => {});
-    },
-    async validate() {
-      return new Promise(async resolve => {
-        let subformItems = this.$refs['subformItem'] || [];
-        if(!_.isArray(subformItems)) { // length === 1
-          subformItems = [subformItems];
-        }
-
-        const valids = await Promise.all(subformItems.map(subform => {
-          return subform.validate();
-        }));
-        let valiResult = true;
-        valids.forEach(valid => {
-          valiResult = valiResult && valid;
-        })
-        return resolve(valiResult);
-      });
-    }
-  }
+  mixins: [formItemMixin, itemListMixin],
 }
 </script>
--- src/ui-kit/element-ui/components/item-list/item-list3.vue
+++ src/ui-kit/element-ui/components/item-list/item-list3.vue
@@ -69,9 +69,8 @@
 </style>
 
 <script>
-import _ from 'lodash';
-
 import formItemMixin from '../../common/form-item/mixin'
+import itemListMixin from '../../../common/components/item-list-mixin'
 
 import DraggableList from '../../../common/draggable-list3'
 import DraggableListItem from '../../../common/draggable-list-item'
@@ -84,89 +83,6 @@ export default {
     DraggableListItem,
     Subform
   },
-  mixins: [formItemMixin],
-  props: {
-    schema: {
-      type: Object,
-      default() {
-        return {};
-      }
-    }
-  },
-  computed: {
-    slotNames() {
-      const $root = this.supNodes[0];
-      let slotNames = Object.keys($root.$slots || {});
-      slotNames = slotNames.concat(Object.keys($root.$scopedSlots || {}));
-      return _.uniq(slotNames);
-    },
-    subformMarginY() {
-      return this.schema.formConf.marginY || 15;
-    },
-    editable() {
-      return !this.readonly && !this.disabled;
-    },
-    subformField() {
-      return {
-        ...this.field,
-        effect: undefined,
-      };
-    },
-    subformSchema() {
-      return {
-        ...this.field,
-        type: 'subform',
-      };
-    },
-    items: {
-      get() {
-        return this.data[this.field.name];
-      },
-      set(items) {
-        this.$set(this.data, this.field.name, items);
-      }
-    }
-  },
-  created() {
-    if(!this.items) {
-      this.items = [];
-    }
-  },
-  methods: {
-    addItem() {
-      this.items = [...this.items, {}];
-    },
-    deleteItem(item) {
-      this.$confirm('确认删除?', null, {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'danger',
-      }).then(() => {
-        this.items = this.items.filter(i => i !== item);
-
-        this.$message({
-          type: 'success',
-          message: '已删除',
-        });
-      }).catch(() => {});
-    },
-    async validate() {
-      return new Promise(async resolve => {
-        let subformItems = this.$refs['subformItem'] || [];
-        if(!_.isArray(subformItems)) { // length === 1
-          subformItems = [subformItems];
-        }
-
-        const valids = await Promise.all(subformItems.map(subform => {
-          return subform.validate();
-        }));
-        let valiResult = true;
-        valids.forEach(valid => {
-          valiResult = valiResult && valid;
-        })
-        return resolve(valiResult);
-      });
-    }
-  }
+  mixins: [formItemMixin, itemListMixin],
 }
 </script>
