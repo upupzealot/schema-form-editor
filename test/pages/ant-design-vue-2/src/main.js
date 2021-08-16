e54bbaf491b54dcfdd33a0fc6443de68bed854c0
upupzealot <hzxjhspxn@163.com>
2021-08-16 18:41:28 +0800

    test: fix ant-design-2 css 引用错误

--- test/pages/ant-design-vue-2/src/main.js
+++ test/pages/ant-design-vue-2/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './app.vue'
 
 import AntDesign from 'ant-design-vue2'
-import 'ant-design-vue1/dist/antd.css'
+import 'ant-design-vue2/dist/antd.css'
 
 const app = createApp(App);
 app.use(AntDesign);
