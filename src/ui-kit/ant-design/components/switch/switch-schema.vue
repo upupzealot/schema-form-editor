ce5001b0332dbb84770550f1353c497271f3972b
caiyansong <caiyansong@enn.cn>
2022-05-10 13:15:04 +0800

    feat: antd switch 值 智能类型

--- src/ui-kit/ant-design/components/switch/switch-schema.vue
+++ src/ui-kit/ant-design/components/switch/switch-schema.vue
@@ -3,13 +3,22 @@
     <template v-slot:basic>
       <el-row :gutter="20">
         <el-col :span="12">
+          <el-form-item label="智能类型">
+            <el-switch
+              v-model="typeValue"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12">
           <el-form-item label="开-键">
             <el-input v-model="field.activeText" />
           </el-form-item>
         </el-col>
         <el-col :span="12">
           <el-form-item label="开-值">
-            <el-input v-model="field.activeValue" />
+            <el-input v-model="activeValue" />
           </el-form-item>
         </el-col>
         <el-col :span="12">
@@ -19,7 +28,7 @@
         </el-col>
         <el-col :span="12">
           <el-form-item label="关-值">
-            <el-input v-model="field.inactiveValue" />
+            <el-input v-model="inactiveValue" />
           </el-form-item>
         </el-col>
       </el-row>
@@ -33,5 +42,52 @@ import schemaItemMixin from '@/framework/schema-item/mixin';
 export default {
   type: 'switch',
   mixins: [schemaItemMixin],
+  computed: {
+    typeValue: {
+      get() {
+        return !!this.field.typeValue;
+      },
+      set(val) {
+        this.$set(this.field, 'typeValue', !!val ? true : undefined);
+        this.setValue('activeValue');
+        this.setValue('inactiveValue');
+      }
+    },
+    activeValue: {
+      get() {
+        return `${this.field.activeValue}`
+      },
+      set(val) {
+        this.setValue('activeValue', val);
+      }
+    },
+    inactiveValue: {
+      get() {
+        return `${this.field.inactiveValue}`
+      },
+      set(val) {
+        this.setValue('inactiveValue', val);
+      }
+    },
+  },
+  methods: {
+    setValue(key, val) {
+      if (this.field.typeValue) {
+        this.$set(this.field, key, val || this.transValue(this.field[key]));
+      } else {
+        this.$set(this.field, key, `${val || this.field[key]}`);
+      }
+    },
+    transValue(val) {
+      if(`${val}` === 'true' || `${val}` === 'false') {
+        return JSON.parse(val);
+      }
+      const numVal = Number(val)
+      if(numVal || numVal === 0) {
+        return numVal;
+      }
+      return val;
+    },
+  }
 };
 </script>
